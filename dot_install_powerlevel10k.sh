102b048245219ffa2d895d6443c9241fda9d46e8
Marco Orfei <marcoasrorfei@gmail.com>
2020-04-28 09:51:22 +0200

    Add powerlevel10k script

---
+++ dot_install_powerlevel10k.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+SSHDIR="~/.ssh"
+if [! -d "$SSHDIR"] ; then
+	mkdir "$SSHDIR"
+fi
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
+mv ~/.zshrc.bck ~/.zshrc
+source ~/.zshrc
--- dot_run_on_ubuntu.sh
+++ dot_run_on_ubuntu.sh
@@ -1,20 +1,15 @@
 #!/bin/sh
 set -e
-sudo apt install git vim build-essential cmake python3-dev zsh || true
+sudo apt install git vim build-essential cmake python3-dev zsh fonts-powerline || true
 git clone --depth=1 https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 git clone --depth=1 https://github.com/ryanoasis/nerd-fonts.git
-cd nerd-fonts && ./install.sh Meslo
-cd ~
-vim +PluginInstall +qall
+cd nerd-fonts && ./install.sh Meslo 
+cd ~ && vim +PluginInstall +qall
 cd ~/.vim/bundle/youcompleteme
 python3 install.py
 if hash zsh > /dev/null 2>&1 ; then
-        cd ~
-        mv ~/.zshrc ~/.zshrc.bck
+        cd ~ && mv ~/.zshrc ~/.zshrc.bck
         sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-        git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
-        mv ~/.zshrc.bck ~/.zshrc
-        source ~/.zshrc
 else
         echo "zsh command not found"
 fi
