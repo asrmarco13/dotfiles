f2c29ada919b6bc1916fcb1602fea1aee14b7b02
Marco Orfei <marcoasrorfei@gmail.com>
2020-04-28 14:25:40 +0200

    Update powerlevel10k script

--- dot_install_powerlevel10k.sh
+++ dot_install_powerlevel10k.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
-if [[ "$OSTYPE" == "linux-gnu" ]] ; then
+if [ "$OSTYPE" == "linux-gnu" ] ; then
 	SSHDIR="~/.ssh"
-	if [! -d "$SSHDIR"] ; then
-        	mkdir "$SSHDIR"
+	if [ ! -d "$SSHDIR" ] ; then
+        	mkdir ~/.ssh
 	fi
-	cd ~ && git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
-	mv ~/.zshrc.bck ~/.zshrc
-	source ~/.zshrc
+	if [ -f /etc/arch-release ] || [ -f /etc/debian_version ] ; then
+		cd ~ && git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
+		mv ~/.zshrc.bck ~/.zshrc
+		source ~/.zshrc
+	else
+		echo "Distro not recognized"
+	fi
 else
-	echo "OS type not recognized"
+	echo "OS type not Linux"
 fi
