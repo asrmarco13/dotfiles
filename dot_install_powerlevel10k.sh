fa994916db7a677bc77962e75393be3804d8cfd2
Marco Orfei <marcoasrorfei@gmail.com>
2020-04-28 16:33:40 +0200

    Update powerlevel10k script

--- dot_install_powerlevel10k.sh
+++ dot_install_powerlevel10k.sh
@@ -7,7 +7,6 @@ if [ "$OSTYPE" == "linux-gnu" ] ; then
 	if [ -f /etc/arch-release ] || [ -f /etc/debian_version ] ; then
 		cd ~ && git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
 		mv ~/.zshrc.bck ~/.zshrc
-		source ~/.zshrc
 	else
 		echo "Distro not recognized"
 	fi
